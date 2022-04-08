96bb71d11ef3f0b5df36beb8e30c4d3326764d41
Brayan Vargas <brayanvargas.rojo@gmail.com>
2022-04-08 14:33:15 -0500

    Se crean las paginas para trabajar la vista del login, se incluye el controlador de Historia

---
+++ api/app/Http/Controllers/Api/V1/HistoriaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Historia;
+use Illuminate\Http\Request;
+
+class HistoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Historia  $historia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Historia $historia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Historia  $historia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Historia $historia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Historia  $historia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Historia $historia)
+    {
+        //
+    }
+}
---
+++ inprocost/src/app/pages/auth/sing-in/sing-in.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sing-in',
+  template: `
+    <app-form></app-form>
+  `,
+  styleUrls: ['./sing-in.component.css']
+})
+export class SingInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
